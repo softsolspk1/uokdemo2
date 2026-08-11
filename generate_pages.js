8c462f3ff1674c9141005753bf196125e97aab30
softsolspk1 <kashiffareed2022@gmail.com>
2026-08-11 13:39:24 +0500

    Add ICCBS sub-menu to update scripts

--- generate_pages.js
+++ generate_pages.js
@@ -150,7 +150,38 @@ const newMobile = `<div class="th-mobile-menu">
         <li class="menu-item-has-children">
           <a href="research.html">Research</a>
           <ul class="sub-menu">
-            <li><a href="research.html">Research Institutes</a></li>
+            <li><a href="research.html">Research Institutes Overview</a></li>
+            <li class="menu-item-has-children">
+              <a href="https://iccs.edu/" target="_blank">International Center for Chemical and Biological Sciences (ICCBS)</a>
+              <ul class="sub-menu">
+                <li><a href="#">H.E.J. Research Institute of Chemistry</a></li>
+                <li><a href="#">Dr. Panjwani Center for Molecular Medicine &amp; Drug Research</a></li>
+                <li><a href="#">Other Chemistry Centers</a></li>
+                <li><a href="#">Other Biological Sciences Related Centers</a></li>
+                <li><a href="#">Third World Center for Science and Technology</a></li>
+                <li><a href="#">Center for Bioequivalence Studies and Clinical Research (CBSCR)</a></li>
+                <li><a href="#">Industrial Analytical Center (IAC)</a></li>
+                <li><a href="#">Biotechnology Wing</a></li>
+                <li><a href="#">Prof. Dr. Wolfgang Voelter Laboratories Complex (NRL)</a></li>
+                <li><a href="#">Welcome to National Facility for Laboratory Animal Research and Care (NFLARC)</a></li>
+                <li><a href="#">L.E.J. Nanotechnology Center</a></li>
+                <li><a href="#">Technology Park &amp; Technology Incubation Center</a></li>
+                <li><a href="#">Latif Ebrahim Jamal National Science Information Center</a></li>
+              </ul>
+            </li>
+            <li><a href="institute-ies.html">Institute of Environmental Studies</a></li>
+            <li><a href="institute-ims.html">Institute of Marine Science</a></li>
+            <li><a href="institute-isst.html">Institute of Space Science &amp; Technology</a></li>
+            <li><a href="institute-nnrc.html">National Nematological Research Centre</a></li>
+            <li><a href="institute-cdfst.html">Center for Digital Forensic Science &amp; Technology</a></li>
+            <li><a href="institute-asce.html">Area Study Center for Europe</a></li>
+            <li><a href="institute-cmg.html">Centre for Molecular Genetics</a></li>
+            <li><a href="institute-cews.html">Centre of Excellence for Women's Studies</a></li>
+            <li><a href="institute-chwb.html">Center for Health &amp; Wellbeing</a></li>
+            <li><a href="institute-psc.html">Pakistan Study Center</a></li>
+            <li><a href="institute-confucius.html">Confucius Institute</a></li>
+            <li><a href="institute-sympdc.html">Sardar Yasin Malik Professional Development Centre</a></li>
+            <li><a href="institute-smbbc.html">Shaheed Mohtarma Benazir Bhutto Chair</a></li>
             <li><a href="institute-kibge.html">Dr. A.Q. Khan Institute (KIBGE)</a></li>
             <li><a href="journals.html">Academic Journals</a></li>
           </ul>
@@ -233,7 +264,38 @@ const newDesktop = `<nav class="main-menu d-none d-xl-block">
                     <li class="menu-item-has-children">
                       <a href="research.html">Research</a>
                       <ul class="sub-menu">
-                        <li><a href="research.html">Research Institutes</a></li>
+                        <li><a href="research.html">Research Institutes Overview</a></li>
+                        <li class="menu-item-has-children">
+                          <a href="https://iccs.edu/" target="_blank">International Center for Chemical and Biological Sciences (ICCBS)</a>
+                          <ul class="sub-menu">
+                            <li><a href="#">H.E.J. Research Institute of Chemistry</a></li>
+                            <li><a href="#">Dr. Panjwani Center for Molecular Medicine &amp; Drug Research</a></li>
+                            <li><a href="#">Other Chemistry Centers</a></li>
+                            <li><a href="#">Other Biological Sciences Related Centers</a></li>
+                            <li><a href="#">Third World Center for Science and Technology</a></li>
+                            <li><a href="#">Center for Bioequivalence Studies and Clinical Research (CBSCR)</a></li>
+                            <li><a href="#">Industrial Analytical Center (IAC)</a></li>
+                            <li><a href="#">Biotechnology Wing</a></li>
+                            <li><a href="#">Prof. Dr. Wolfgang Voelter Laboratories Complex (NRL)</a></li>
+                            <li><a href="#">Welcome to National Facility for Laboratory Animal Research and Care (NFLARC)</a></li>
+                            <li><a href="#">L.E.J. Nanotechnology Center</a></li>
+                            <li><a href="#">Technology Park &amp; Technology Incubation Center</a></li>
+                            <li><a href="#">Latif Ebrahim Jamal National Science Information Center</a></li>
+                          </ul>
+                        </li>
+                        <li><a href="institute-ies.html">Institute of Environmental Studies</a></li>
+                        <li><a href="institute-ims.html">Institute of Marine Science</a></li>
+                        <li><a href="institute-isst.html">Institute of Space Science &amp; Technology</a></li>
+                        <li><a href="institute-nnrc.html">National Nematological Research Centre</a></li>
+                        <li><a href="institute-cdfst.html">Center for Digital Forensic Science &amp; Technology</a></li>
+                        <li><a href="institute-asce.html">Area Study Center for Europe</a></li>
+                        <li><a href="institute-cmg.html">Centre for Molecular Genetics</a></li>
+                        <li><a href="institute-cews.html">Centre of Excellence for Women's Studies</a></li>
+                        <li><a href="institute-chwb.html">Center for Health &amp; Wellbeing</a></li>
+                        <li><a href="institute-psc.html">Pakistan Study Center</a></li>
+                        <li><a href="institute-confucius.html">Confucius Institute</a></li>
+                        <li><a href="institute-sympdc.html">Sardar Yasin Malik Professional Development Centre</a></li>
+                        <li><a href="institute-smbbc.html">Shaheed Mohtarma Benazir Bhutto Chair</a></li>
                         <li><a href="institute-kibge.html">Dr. A.Q. Khan Institute (KIBGE)</a></li>
                         <li><a href="journals.html">Academic Journals</a></li>
                       </ul>
--- update_menus_bulk.py
+++ update_menus_bulk.py
@@ -11,6 +11,24 @@ research_new_desktop = """<li class="menu-item-has-children">
                       <a href="research.html">Research</a>
                       <ul class="sub-menu">
                         <li><a href="research.html">Research Institutes Overview</a></li>
+                        <li class="menu-item-has-children">
+                          <a href="https://iccs.edu/" target="_blank">International Center for Chemical and Biological Sciences (ICCBS)</a>
+                          <ul class="sub-menu">
+                            <li><a href="#">H.E.J. Research Institute of Chemistry</a></li>
+                            <li><a href="#">Dr. Panjwani Center for Molecular Medicine &amp; Drug Research</a></li>
+                            <li><a href="#">Other Chemistry Centers</a></li>
+                            <li><a href="#">Other Biological Sciences Related Centers</a></li>
+                            <li><a href="#">Third World Center for Science and Technology</a></li>
+                            <li><a href="#">Center for Bioequivalence Studies and Clinical Research (CBSCR)</a></li>
+                            <li><a href="#">Industrial Analytical Center (IAC)</a></li>
+                            <li><a href="#">Biotechnology Wing</a></li>
+                            <li><a href="#">Prof. Dr. Wolfgang Voelter Laboratories Complex (NRL)</a></li>
+                            <li><a href="#">Welcome to National Facility for Laboratory Animal Research and Care (NFLARC)</a></li>
+                            <li><a href="#">L.E.J. Nanotechnology Center</a></li>
+                            <li><a href="#">Technology Park &amp; Technology Incubation Center</a></li>
+                            <li><a href="#">Latif Ebrahim Jamal National Science Information Center</a></li>
+                          </ul>
+                        </li>
                         <li><a href="institute-ies.html">Institute of Environmental Studies</a></li>
                         <li><a href="institute-ims.html">Institute of Marine Science</a></li>
                         <li><a href="institute-isst.html">Institute of Space Science &amp; Technology</a></li>
@@ -33,6 +51,24 @@ research_new_mobile = """<li class="menu-item-has-children">
           <a href="research.html">Research</a>
           <ul class="sub-menu">
             <li><a href="research.html">Research Institutes Overview</a></li>
+            <li class="menu-item-has-children">
+              <a href="https://iccs.edu/" target="_blank">International Center for Chemical and Biological Sciences (ICCBS)</a>
+              <ul class="sub-menu">
+                <li><a href="#">H.E.J. Research Institute of Chemistry</a></li>
+                <li><a href="#">Dr. Panjwani Center for Molecular Medicine &amp; Drug Research</a></li>
+                <li><a href="#">Other Chemistry Centers</a></li>
+                <li><a href="#">Other Biological Sciences Related Centers</a></li>
+                <li><a href="#">Third World Center for Science and Technology</a></li>
+                <li><a href="#">Center for Bioequivalence Studies and Clinical Research (CBSCR)</a></li>
+                <li><a href="#">Industrial Analytical Center (IAC)</a></li>
+                <li><a href="#">Biotechnology Wing</a></li>
+                <li><a href="#">Prof. Dr. Wolfgang Voelter Laboratories Complex (NRL)</a></li>
+                <li><a href="#">Welcome to National Facility for Laboratory Animal Research and Care (NFLARC)</a></li>
+                <li><a href="#">L.E.J. Nanotechnology Center</a></li>
+                <li><a href="#">Technology Park &amp; Technology Incubation Center</a></li>
+                <li><a href="#">Latif Ebrahim Jamal National Science Information Center</a></li>
+              </ul>
+            </li>
             <li><a href="institute-ies.html">Institute of Environmental Studies</a></li>
             <li><a href="institute-ims.html">Institute of Marine Science</a></li>
             <li><a href="institute-isst.html">Institute of Space Science &amp; Technology</a></li>
